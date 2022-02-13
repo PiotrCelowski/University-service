874a1c61f40def10f651472f4a19e4c2d8b511d1
Piotr Celowski <piotr.celowski@gmail.com>
2022-02-13 13:26:53 +0100

    Add user/group functionality and correct ui for other subdomains

---
+++ src/main/java/university/service/ui/programs/forms/EventForm.java
@@ -0,0 +1,88 @@
+package university.service.ui.programs.forms;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
+import university.service.domain.program.ProgramEntity;
+
+public class ProgramForm extends FormLayout {
+    TextField programName = new TextField("Program Name");
+    TextField programDescription = new TextField("Program Description");
+
+    private ProgramEntity programEntity;
+    Binder<ProgramEntity> binder = new BeanValidationBinder<>(ProgramEntity.class);
+
+    Button save = new Button("Save");
+
+    public ProgramForm() {
+        addClassName("program-form");
+        binder.bindInstanceFields(this);
+        programName.setEnabled(true);
+        programName.setReadOnly(false);
+        programDescription.setEnabled(true);
+        programDescription.setReadOnly(false);
+
+        add(programName, programDescription, createButtonsLayout());
+    }
+
+    private HorizontalLayout createButtonsLayout() {
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        save.addClickShortcut(Key.ENTER);
+
+        save.addClickListener(event -> validateAndSave());
+
+        save.addClickShortcut(Key.ENTER);
+
+        binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+
+        return new HorizontalLayout(save);
+    }
+
+    public void setProgramEntity(ProgramEntity programEntity) {
+        this.programEntity = programEntity;
+        binder.readBean(this.programEntity);
+    }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(this.programEntity);
+            fireEvent(new SaveEvent(this, this.programEntity));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Events
+    public abstract static class ProgramFormEvent extends ComponentEvent<ProgramForm> {
+        private ProgramEntity programEntity;
+
+        public ProgramFormEvent(ProgramForm source, ProgramEntity programEntity) {
+            super(source, true);
+            this.programEntity = programEntity;
+        }
+
+        public ProgramEntity getProgramEntity() {
+            return programEntity;
+        }
+    }
+
+    public static class SaveEvent extends ProgramFormEvent {
+        SaveEvent(ProgramForm source, ProgramEntity programEntity) {
+            super(source, programEntity);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType, ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
+}
---
+++ src/main/java/university/service/ui/programs/forms/GradeForm.java
@@ -0,0 +1,83 @@
+package university.service.ui.programs.forms;
+
+import com.vaadin.flow.component.ComponentEvent;
+import com.vaadin.flow.component.ComponentEventListener;
+import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.binder.BeanValidationBinder;
+import com.vaadin.flow.data.binder.Binder;
+import com.vaadin.flow.data.binder.ValidationException;
+import com.vaadin.flow.shared.Registration;
+import university.service.domain.event.EventEntity;
+import university.service.domain.grade.GradeEntity;
+import university.service.domain.program.ProgramEntity;
+import university.service.domain.program.SubjectEntity;
+
+public class EventForm extends FormLayout {
+    TextField eventDate = new TextField("Event date");
+    private EventEntity selectedEvent;
+
+    Binder<EventEntity> binder = new BeanValidationBinder<>(EventEntity.class);
+
+    Button save = new Button("Save");
+
+    public EventForm() {
+        addClassName("program-form");
+        binder.bindInstanceFields(this);
+        eventDate.setEnabled(true);
+        eventDate.setReadOnly(false);
+        add(eventDate, createButtonsLayout());
+    }
+
+    private HorizontalLayout createButtonsLayout() {
+        save.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        save.addClickShortcut(Key.ENTER);
+        save.addClickListener(event -> validateAndSave());
+        save.addClickShortcut(Key.ENTER);
+        binder.addStatusChangeListener(e -> save.setEnabled(binder.isValid()));
+        return new HorizontalLayout(save);
+    }
+
+    public void setSelectedEvent(EventEntity event) {
+        this.selectedEvent = event;
+        binder.readBean(this.selectedEvent);
+    }
+
+    private void validateAndSave() {
+        try {
+            binder.writeBean(this.selectedEvent);
+            fireEvent(new SaveEvent(this, this.selectedEvent));
+        } catch (ValidationException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //Events
+    public abstract static class EventFormEvent extends ComponentEvent<EventForm> {
+        private EventEntity eventEntity;
+
+        public EventFormEvent(EventForm source, EventEntity eventEntity) {
+            super(source, true);
+            this.eventEntity = eventEntity;
+        }
+
+        public EventEntity getEventEntity() {
+            return this.eventEntity;
+        }
+    }
+
+    public static class SaveEvent extends EventFormEvent {
+        SaveEvent(EventForm source, EventEntity eventEntity) {
+            super(source, eventEntity);
+        }
+    }
+
+    public <T extends ComponentEvent<?>> Registration addListener(Class<T> eventType, ComponentEventListener<T> listener) {
+        return getEventBus().addListener(eventType, listener);
+    }
+
+}
